c927293b490d9f3c4f0d3e0327b673387825d019
Frankie Moran <frankie.jack.moran@gmail.com>
2026-09-02 14:54:11 +0100

    test(PO-9803): stabilise failed countries banners

--- cypress/component/createDraftCasefile/ApplicantOrganisation.cy.ts
+++ cypress/component/createDraftCasefile/ApplicantOrganisation.cy.ts
@@ -705,7 +705,7 @@ describe('Create Casefile Applicant Organisation', () => {
       cy.get(Page.caseDetails.applicantLink).click();
       cy.wait('@getCountries').its('response.statusCode').should('equal', 503);
       assertRouterPath(taskListPath);
-      cy.get('@globalStore').then((globalStore: GlobalStoreInstance) => {
+      cy.get('@globalStore').should((globalStore: GlobalStoreInstance) => {
         expect(globalStore.bannerError()).to.deep.equal({
           error: true,
           title: problem.title,

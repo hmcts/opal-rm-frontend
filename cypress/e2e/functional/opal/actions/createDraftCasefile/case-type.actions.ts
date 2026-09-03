2ecb91e754feee80a1b457bb85d412f38804b1cd
jonathanDuffy <jonathan.duffy@cgi.com>
2026-09-03 09:57:11 +0100

    test: open Cases dashboard before casefile

--- cypress/e2e/functional/opal/actions/createDraftCasefile/case-type.actions.ts
+++ cypress/e2e/functional/opal/actions/createDraftCasefile/case-type.actions.ts
@@ -1,4 +1,5 @@
 import { CreateCasefileSelectors as Page } from 'cypress/shared/selectors/create-casefile.selectors';
+import { PrimaryNavigationLocators as Navigation } from 'cypress/shared/selectors/primary-navigation.locators';
 import { createScopedLogger } from 'cypress/support/utils/log.helper';
 
 const log = createScopedLogger('CaseTypeActions');
@@ -8,6 +9,8 @@ export class CaseTypeActions {
   /** Starts the agreed case path from the authenticated Cases dashboard. */
   public startRemoInIndividual(): void {
     log('action', 'Starting a REMO In case with an Individual applicant');
+    cy.contains(Navigation.items, Navigation.labels.cases).should('be.visible').click();
+    cy.location('pathname').should('eq', '/dashboard/cases');
     cy.get(Page.dashboard.createCasefileLink, { timeout: 20_000 }).should('be.visible').click();
     cy.location('pathname').should('eq', '/cases/create-casefile/case-type');
     cy.get(Page.caseTypeRadio('REMO In')).check();
